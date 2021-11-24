8817545528cfdf96eb2e732d23f5feba9c6d32b0
Dmitrii Golubenko <d.golubenko@knights.ucf.edu>
2021-11-24 16:24:33 -0500

    Closes #1, #2, #3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,64 +1,60 @@
-import mainContext from '../context/context';
-import Form from 'react-bootstrap/Form'
+import mainContext from '../context/MainContext';
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Home.css'
+import './Home.css';
 
 import { useContext } from 'react';
 
-function SubmitHandler (event: any): void 
-{
-    const context = useContext(mainContext);
+function SubmitHandler(event: any): void {
+  const context = useContext(mainContext);
 
-    // prevents submit button to reload page
-    event.preventDefault();
-    context.logout();
+  // prevents submit button to reload page
+  event.preventDefault();
+  context.logout();
 }
 
-function HomePage(): JSX.Element 
-{
+const HomePage = function (): JSX.Element {
+  const context = useContext(mainContext);
+  return (
+    <header>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
+      <link rel="stylesheet" href="Home.css" />
+      <div className="home-page">
+        <h1 className="username-display">{context.var1}</h1>
 
-    const context = useContext(mainContext);
-    return (
-        <header>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
-        <link rel="stylesheet" href="Home.css"></link>
-            <div className="home-page">
-                <h1 className="username-display">{context.var1}</h1>
-
-                <Form className = "home-form" onSubmit={SubmitHandler}>
-                    <Button variant="primary" className="module-button" type="submit">
-                        Events
-                    </Button>
-                    <Button variant="primary" className="module-button" type="submit">
-                        Chat
-                    </Button>
-                    <Button variant="primary" className="module-button" type="submit">
-                        PDF's
-                    </Button>
-                    <Button variant="primary" className="module-button" type="submit">
-                        Notification
-                    </Button>
-                    <Button variant="primary" className="module-button" type="submit">
-                        Calendar
-                    </Button>
-                    <Button variant="primary" className="module-button" type="submit">
-                        Module
-                    </Button>
-                    <Button variant="primary" className="module-button" type="submit">
-                        Module
-                    </Button>
-                    <Button variant="primary" className="module-button" type="submit">
-                        Test
-                    </Button>
-                    <Button variant="primary" className="module-button" type="submit">
-                        Test
-                    </Button>
-                    
-                </Form>
-            </div>
-        </header>
-    );
-}
+        <Form className="home-form" onSubmit={SubmitHandler}>
+          <Button variant="primary" className="module-button" type="submit">
+            Events
+          </Button>
+          <Button variant="primary" className="module-button" type="submit">
+            Chat
+          </Button>
+          <Button variant="primary" className="module-button" type="submit">
+            PDF's
+          </Button>
+          <Button variant="primary" className="module-button" type="submit">
+            Notification
+          </Button>
+          <Button variant="primary" className="module-button" type="submit">
+            Calendar
+          </Button>
+          <Button variant="primary" className="module-button" type="submit">
+            Module
+          </Button>
+          <Button variant="primary" className="module-button" type="submit">
+            Module
+          </Button>
+          <Button variant="primary" className="module-button" type="submit">
+            Test
+          </Button>
+          <Button variant="primary" className="module-button" type="submit">
+            Test
+          </Button>
+        </Form>
+      </div>
+    </header>
+  );
+};
 
 export default HomePage;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,54 +1,60 @@
-import { useContext } from 'react';
-import React from "react";
-import './Login.css';
-import mainContext from '../context/context';
+import React, { useContext } from 'react';
 
-import Form from 'react-bootstrap/Form'
+import './Login.css';
+
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import mainContext from '../context/MainContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function LoginPage(): JSX.Element 
-{
-    const context = useContext(mainContext);
-    const loginEl = React.useRef<HTMLInputElement>(null);
-    const passwordEl = React.useRef(null);
+const LoginPage = function (): JSX.Element {
+  const context = useContext(mainContext);
+  const loginEl: React.RefObject<HTMLInputElement> = React.useRef<HTMLInputElement>(null);
+  const passwordEl: React.MutableRefObject<null> = React.useRef(null);
 
-    function SubmitHandler (event: React.FormEvent<HTMLFormElement>): void 
-    {
-        // prevents submit button to reload page
-        event.preventDefault();
+  function SubmitHandler(event: React.FormEvent<HTMLFormElement>): void {
+    // prevents submit button to reload page
+    event.preventDefault();
 
-        if (loginEl && loginEl.current)
-        {
-            const login = loginEl.current.value;
-            // @ts-ignore
-            context.login(login);
-        }
+    if (loginEl && loginEl.current) {
+      const login: string = loginEl.current.value;
+      // @ts-ignore
+      context.login(login);
     }
+  }
 
-    return (
-            
-        <div className = "div-background">
-            <div className="header">
-                <h1>Electric Eagles</h1>
-            </div>        
-            <Form className = "login-form" onSubmit={SubmitHandler}>
-            <Form.Group className="mb-3" controlId="login">
-                <Form.Label>Login</Form.Label>
-                <Form.Control type="text" placeholder="Username" id="login" ref={loginEl} required/>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" id="password" ref={passwordEl} required/>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-            
-            </Form>
-        </div>
-
-    );
-}
+  return (
+    <div className="div-background">
+      <div className="header">
+        <h1>Electric Eagles</h1>
+      </div>
+      <Form className="login-form" onSubmit={SubmitHandler}>
+        <Form.Group className="mb-3" controlId="login">
+          <Form.Label>Login</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Username"
+            id="login"
+            ref={loginEl}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            id="password"
+            ref={passwordEl}
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export default LoginPage;
