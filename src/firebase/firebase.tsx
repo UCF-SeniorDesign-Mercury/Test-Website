26d893aa183414eef38dbacb9fb7b11f1a001531
Chris Gray <chrisgrayak@gmail.com>
2022-01-13 18:29:28 -0500

    pulled file from database and displayed on web

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -1,8 +1,13 @@
 import * as firebase from 'firebase/app';
+// import firebase from 'firebase/app'
 import { Auth, getAuth, signInWithEmailAndPassword} from 'firebase/auth';
 '@firebase/auth-types';
 
+import { initializeApp } from 'firebase/app';
+import { getStorage, ref,getBlob, listAll,getMetadata, getBytes, FirebaseStorage } from 'firebase/storage';
+
 import firebaseConfig from './firebaseConfig';
+
 // const axios = require('axios');
 
 // Initialize Firebase App
@@ -36,3 +41,94 @@ export const getToken = async(auth: Auth): Promise<string | undefined> => {
     throw (err);
   }
 };
+
+// export const auth = getAuth();
+// export const registerWithEmail = async (email: string, password: string): Promise<boolean | undefined> =>
+//   auth.createUserWithEmailAndPassword(email, password);
+
+// export const firebaseStorage = async(storage:string): Promise<boolean | undefined> =>
+// {
+//   storage = firebase.storage(firebaseApp);
+//   const files = [ 'RST_Request_Form_Blank.pdf' ]; 
+//   files.map( filename => {
+//       storage
+//         .ref( `/covers/${filename}` )
+//         .getDownloadURL()
+//         .then( url => {
+//           console.log( "Got download url: ", url );
+//         });
+//   });
+// };
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const downloadPDF = async (storage: FirebaseStorage, filename: string): Promise<ArrayBuffer> =>{
+  storage = getStorage(firebaseApp);
+  const storageRef = ref(storage, filename);
+  const PDF = getBytes(storageRef);
+  console.log(PDF);
+  return PDF;
+};
+// Get a reference to the storage service, which is used to create references in your storage bucket
+// const storage = getStorage(firebaseApp);
+// const storageRef = ref(storage);
+// const storageRef_RST_Base64 = ref(storage, 'RST_base64.txt');
+// console.log(storageRef);
+// console.log(storageRef_RST_Base64.storage);
+// const test = storageRef_RST_Base64.toString();
+// console.log(test);
+
+// Get metadata properties
+// getMetadata(storageRef_RST_Base64)
+//   .then((metadata) => {
+//     // Metadata now contains the metadata for 'images/forest.jpg'
+//     console.log(metadata);
+//     return metadata;
+//   })
+//   .catch((error) => {
+//     // Uh-oh, an error occurred!
+//   });
+
+  
+
+// getDownloadURL(ref(storage, 'RST_base64.txt'))
+//   .then((url) => {
+//     // `url` is the download URL for 'images/stars.jpg'
+
+//     // This can be downloaded directly:
+//     const xhr = new XMLHttpRequest();
+//     xhr.responseType = 'blob';
+//     xhr.onload = (event) => {
+//       const blob = xhr.response;
+//       console.log(blob);
+//     };
+//     xhr.open('GET', url);
+//     xhr.send();
+
+//     // Or inserted into an <img> element
+//     const img = document.getElementById('myimg');
+//     img.setAttribute('src', url);
+//     console.log(url);
+//   })
+//   .catch((error) => {
+//     // Handle any errors
+//   });
+
+
+
+
+
+// listAll(storageRef)
+//   .then((res) => {
+//     res.prefixes.forEach((folderRef) => {
+//       // All the prefixes under listRef.
+//       // You may call listAll() recursively on them.
+//       console.log(folderRef);
+//     });
+//     res.items.forEach((itemRef) => {
+//       // All the items under listRef.
+//       console.log(itemRef);
+//     });
+//   }).catch((error) => {
+//     // Uh-oh, an error occurred!
+//   });
--- src/pages/PDF_Test.tsx
+++ src/pages/PDF_Test.tsx
@@ -2,6 +2,7 @@ import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
 import './PDF.css';
+import { downloadPDF } from '../firebase/firebase';
 
 
 const iframeStyle = {
@@ -14,7 +15,7 @@ const PDF_TestPage = function (): JSX.Element {
 
   useEffect(() => {
     async function modifyPdf() {
-      const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf';
+      const url = 'https://firebasestorage.googleapis.com/v0/b/electric-eagles.appspot.com/o/RST_Request_Form_Blank.pdf?alt=media&token=2fbae07a-1496-49ba-ab74-6cf5dd87f9b2';
       const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer());
     
       const pdfDoc = await PDFDocument.load(existingPdfBytes);
@@ -24,14 +25,14 @@ const PDF_TestPage = function (): JSX.Element {
       const firstPage = pages[0];
       // eslint-disable-next-line
       const { width, height } = firstPage.getSize();
-      firstPage.drawText('Testing writing on a form :)', {
-        x: 5,
-        y: height / 2 + 300,
-        size: 50,
-        font: helveticaFont,
-        color: rgb(0.95, 0.1, 0.1),
-        rotate: degrees(-45),
-      });
+      // firstPage.drawText('Testing writing on a form :)', {
+      //   x: 5,
+      //   y: height / 2 + 300,
+      //   size: 50,
+      //   font: helveticaFont,
+      //   color: rgb(0.95, 0.1, 0.1),
+      //   rotate: degrees(-45),
+      // });
 
       const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true })
         .catch(err => {
@@ -46,6 +47,7 @@ const PDF_TestPage = function (): JSX.Element {
     
       // eslint-disable-next-line
       const pdfBytes = await pdfDoc.save();
+      console.log(pdfBytes);
     }
     modifyPdf()
       .catch(err => console.log(err))
