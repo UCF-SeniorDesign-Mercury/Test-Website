513e1f39a205b23f025094ec501e0e292fbbc819
Chris Gray <chrisgrayak@gmail.com>
2022-02-08 21:20:28 -0500

    testing new calendar

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,99 +1,24 @@
-import React from 'react';
-import FullCalendar, { EventApi, DateSelectArg, EventClickArg, EventContentArg, formatDate } from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import { INITIAL_EVENTS, createEventId } from './event-utils';
+import { useCallback, useState } from "react";
+import FullCalendar, {
+  DateSelectArg,
+  EventApi,
+  EventClickArg
+} from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import allLocales from "@fullcalendar/core/locales-all";
+import interactionPlugin from "@fullcalendar/interaction";
+import { INITIAL_EVENTS, createEventId } from "./event-utils";
 
-interface CalendarpageState {
-  weekendsVisible: boolean;
-  currentEvents: EventApi[];
-}
-
-export default class Calendarpage extends React.Component<{unknown: any}, CalendarpageState> {
-
-  state: CalendarpageState = {
-    weekendsVisible: true,
-    currentEvents: []
-  }
-
-  render() {
-    return (
-      <div className='demo-app'>
-        {this.renderSidebar()}
-        <div className='demo-app-main'>
-          <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            headerToolbar={{
-              left: 'prev,next today',
-              center: 'title',
-              right: 'dayGridMonth,timeGridWeek,timeGridDay'
-            }}
-            initialView='dayGridMonth'
-            editable={true}
-            selectable={true}
-            selectMirror={true}
-            dayMaxEvents={true}
-            weekends={this.state.weekendsVisible}
-            initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
-            select={this.handleDateSelect}
-            eventContent={renderEventContent} // custom render function
-            eventClick={this.handleEventClick}
-            eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-            /* you can update a remote database when these fire:
-            eventAdd={function(){}}
-            eventChange={function(){}}
-            eventRemove={function(){}}
-            */
-          />
-        </div>
-      </div>
-    );
-  }
-
-  renderSidebar() {
-    return (
-      <div className='demo-app-sidebar'>
-        <div className='demo-app-sidebar-section'>
-          <h2>Instructions</h2>
-          <ul>
-            <li>Select dates and you will be prompted to create a new event</li>
-            <li>Drag, drop, and resize events</li>
-            <li>Click an event to delete it</li>
-          </ul>
-        </div>
-        <div className='demo-app-sidebar-section'>
-          <label>
-            <input
-              type='checkbox'
-              checked={this.state.weekendsVisible}
-              onChange={this.handleWeekendsToggle}
-            ></input>
-            toggle weekends
-          </label>
-        </div>
-        <div className='demo-app-sidebar-section'>
-          <h2>All Events ({this.state.currentEvents.length})</h2>
-          <ul>
-            {this.state.currentEvents.map(renderSidebarEvent)}
-          </ul>
-        </div>
-      </div>
-    );
-  }
-
-  handleWeekendsToggle = () => {
-    this.setState({
-      weekendsVisible: !this.state.weekendsVisible
-    });
-  }
-
-  handleDateSelect = (selectInfo: DateSelectArg) => {
-    const title = prompt('Please enter a new title for your event');
-    const calendarApi = selectInfo.view.calendar;
-
-    calendarApi.unselect(); // clear date selection
-
+const Calendarpage = function (): JSX.Element {
+  const [currentEvents, setCurrentEvents] = useState<EventApi[]>([]);
+  const handleEvents = useCallback(
+    (events: EventApi[]) => setCurrentEvents(events),
+    []
+  );
+  const handleDateSelect = useCallback((selectInfo: DateSelectArg) => {
+    let title = prompt("イベントのタイトルを入力してください")?.trim();
+    let calendarApi = selectInfo.view.calendar;
+    calendarApi.unselect();
     if (title) {
       calendarApi.addEvent({
         id: createEventId(),
@@ -103,36 +28,32 @@ export default class Calendarpage extends React.Component<{unknown: any}, Calend
         allDay: selectInfo.allDay
       });
     }
-  }
-
-  handleEventClick = (clickInfo: EventClickArg) => {
-    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+  }, []);
+  const handleEventClick = useCallback((clickInfo: EventClickArg) => {
+    if (
+      window.confirm(`このイベント「${clickInfo.event.title}」を削除しますか`)
+    ) {
       clickInfo.event.remove();
     }
-  }
-
-  handleEvents = (events: EventApi[]) => {
-    this.setState({
-      currentEvents: events
-    });
-  }
-
-}
-
-function renderEventContent(eventContent: EventContentArg) {
+  }, []);
   return (
-    <>
-      <b>{eventContent.timeText}</b>
-      <i>{eventContent.event.title}</i>
-    </>
+    <div className="demo-app">
+      <div className="demo-app-main">
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          selectable={true}
+          editable={true}
+          initialEvents={INITIAL_EVENTS}
+          locales={allLocales}
+          locale="ja"
+          eventsSet={handleEvents}
+          select={handleDateSelect}
+          eventClick={handleEventClick}
+        />
+      </div>
+    </div>
   );
 }
 
-function renderSidebarEvent(event: EventApi) {
-  return (
-    <li key={event.id}>
-      <b>{formatDate(event.start!, {year: 'numeric', month: 'short', day: 'numeric'})}</b>
-      <i>{event.title}</i>
-    </li>
-  );
-}
+export default Calendarpage;
--- src/pages/event-utils.tsx
+++ src/pages/event-utils.tsx
@@ -1,21 +1,17 @@
-import { EventInput } from '@fullcalendar/react';
+import { EventInput } from "@fullcalendar/react";
 
 let eventGuid = 0;
-const todayStr = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
-
+let todayStr = new Date().toISOString().replace(/T.*$/, ""); // YYYY-MM-DD of today
+export const createEventId = () => String(eventGuid++);
 export const INITIAL_EVENTS: EventInput[] = [
   {
     id: createEventId(),
-    title: 'All-day event',
+    title: "All-day event",
     start: todayStr
   },
   {
     id: createEventId(),
-    title: 'Timed event',
-    start: todayStr + 'T12:00:00'
+    title: "Timed event",
+    start: todayStr + "T12:00:00"
   }
 ];
-
-export function createEventId() {
-  return String(eventGuid++);
-}
