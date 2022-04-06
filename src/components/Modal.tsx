3668584c2f6be2a02e3cb275c9caab1b3f1c06e5
Chris Gray <chrisgrayak@gmail.com>
2022-04-05 21:40:43 -0400

    added custom modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -18,6 +18,19 @@ const style = {
   p: 4,
   overflow: 'auto',
 };
+const eventStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 'auto',
+  maxHeight: 'auto',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 1,
+  overflow: 'auto',
+};
 
 interface CustomModalProps
 {
@@ -37,6 +50,29 @@ export const CustomModal: React.FC<ModalProps & unknown & CustomModalProps> = pr
           <div>
             <IconButton onClick={() => props.setOpen(false)} size='small' sx={{
               left: '90%',
+            }}>
+              <CloseIcon />
+            </IconButton>
+            {props.children}
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+export const CustomEventModal: React.FC<ModalProps & unknown & CustomModalProps> = props => {
+
+  return (
+    <div>
+      <Modal
+        open={(props as ModalProps).open}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={eventStyle}>
+          <div>
+            <IconButton onClick={() => props.setOpen(false)} size='small' sx={{
+              left: '95%',
             }}>
               <CloseIcon />
             </IconButton>
